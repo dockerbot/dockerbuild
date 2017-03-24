6a6f2ec86d819f4113bdb1073de9f5a3d175307a
dockerbot <jamesgrosz@gmail.com>
2017-03-24 16:22:25 +0100

    Update test.php

--- phpcode/test.php
+++ phpcode/test.php
@@ -1,5 +1,15 @@
 <?php
 
-echo "bonjour le monde!";
+$dsn = 'mysql:host=IP_ADRESSE';
+$usr = 'root';
+$pwd = 'password';
+
+try {
+    $dbh = new PDO($dsn, $usr, $pwd);
+} catch (PDOException $e) {
+    die('Connection failed: ' . $e->getMessage());
+}
+
+echo 'Le connection est fonctionnelle! :)';
 
 ?>
